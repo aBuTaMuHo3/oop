bc860a2f7084a860c8ccfaff2b335795f02182b4
aBuTaMuHo3 <gallardo.spider@mail.ru>
2016-12-07 09:25:52 +0300

    added lab 5

---
+++ lab5/task1/Rational.cpp
@@ -0,0 +1,232 @@
+﻿#include "stdafx.h"
+#include "Rational.h"
+#include <utility>
+#include <stdexcept>
+
+
+CRational::CRational(int numerator, int denominator)
+	: m_numerator(numerator)
+	, m_denominator(denominator)
+{
+	if (denominator == 0)
+	{
+		throw std::invalid_argument("Denominator must not be equal to zero");
+	}
+	if (denominator < 0)
+	{
+		m_numerator = -m_numerator;
+		m_denominator = -m_denominator;
+	}
+	Normalize();
+}
+
+int CRational::GetNumerator() const
+{
+	return m_numerator;
+}
+
+int CRational::GetDenominator() const
+{
+	return m_denominator;
+}
+
+void CRational::Normalize()
+{
+	const int gcd = GCD(abs(m_numerator), m_denominator);
+	m_numerator /= gcd;
+	m_denominator /= gcd;
+}
+
+unsigned GCD(unsigned a, unsigned b)
+{
+	while (b != 0)
+	{
+		std::swap(a, b);
+		b = b % a;
+	}
+	return (a != 0) ? a : 1;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 1. Реализовать метод ToDouble() согласно заданию
+//////////////////////////////////////////////////////////////////////////
+
+double CRational::ToDouble() const
+{
+	return static_cast<double> (m_numerator) / (m_denominator);
+}
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 2. Реализовать унарный + и унарный -
+//////////////////////////////////////////////////////////////////////////
+
+CRational const CRational::operator +() const
+{
+	return *this;
+}
+
+CRational const CRational::operator -()
+{
+	m_numerator = -m_numerator;
+	return *this;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 3. Реализовать бинарный +
+//////////////////////////////////////////////////////////////////////////
+
+CRational const operator+(CRational const& first, CRational const& second)
+{
+	int numerator = first.GetNumerator() * second.GetDenominator() + first.GetDenominator() * second.GetNumerator();
+	int denomirator = first.GetDenominator() * second.GetDenominator();
+	return CRational(numerator, denomirator);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 4. Реализовать бинарный -
+//////////////////////////////////////////////////////////////////////////
+
+
+CRational const operator-(CRational const& first, CRational const& second)
+{
+	int numerator = first.GetNumerator() * second.GetDenominator() - first.GetDenominator() * second.GetNumerator();
+	int denomirator = first.GetDenominator() * second.GetDenominator();
+	return CRational(numerator, denomirator);
+}
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 5. Реализовать оператор +=
+//////////////////////////////////////////////////////////////////////////
+
+CRational const CRational::operator +=(CRational const& first)
+{
+	*this = *this + first;
+	return *this;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 6. Реализовать оператор -=
+//////////////////////////////////////////////////////////////////////////
+
+CRational const CRational::operator -=(CRational const& first)
+{
+	*this = *this - first;
+	return *this;
+}
+
+CRational const operator *(CRational const& first, CRational const& second)
+{
+	return CRational((first.GetNumerator() * second.GetNumerator()),
+					 (first.GetDenominator() * second.GetDenominator()));
+}
+
+CRational const operator /(CRational const& first, CRational const& second)
+{
+	return CRational((first.GetNumerator() * second.GetDenominator()),
+		(first.GetDenominator() * second.GetNumerator()));
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 9. Реализовать оператор *=
+//////////////////////////////////////////////////////////////////////////
+
+
+CRational const CRational::operator *=(CRational const& first)
+{
+	*this = *this * first;
+	return *this;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 10. Реализовать оператор /=
+//////////////////////////////////////////////////////////////////////////
+
+CRational const CRational::operator /=(CRational const& first)
+{
+	*this = *this / first;
+	return *this;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 11. Реализовать операторы == и !=
+//////////////////////////////////////////////////////////////////////////
+
+bool const operator ==(CRational const& first, CRational const& second)
+{
+	return (first.GetNumerator() == second.GetNumerator()) &&
+		(first.GetDenominator() == second.GetDenominator());
+}
+
+bool const operator !=(CRational const& first, CRational const& second)
+{
+	return (first.GetNumerator() != second.GetNumerator()) ||
+		(first.GetDenominator() != second.GetDenominator());
+}
+//////////////////////////////////////////////////////////////////////////
+// TODO: 12. Реализовать операторы <, >, <=, >=
+//////////////////////////////////////////////////////////////////////////
+
+bool const operator <(CRational const& first, CRational const& second)
+{
+	return ((first.GetNumerator() * second.GetDenominator()) < (first.GetDenominator() * second.GetNumerator()));
+}
+
+
+
+bool const operator >(CRational const& first, CRational const& second)
+{
+	return ((first.GetNumerator() * second.GetDenominator()) > (first.GetDenominator() * second.GetNumerator()));
+}
+bool const operator <=(CRational const& first, CRational const& second)
+{
+	return !(first > second);
+}
+
+bool const operator >=(CRational const& first, CRational const& second)
+{
+	return !(first < second);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 13. Реализовать оператор вывода рационального числа в выходной поток 
+//////////////////////////////////////////////////////////////////////////
+
+std::ostream & operator <<(std::ostream &stream, CRational const& rational)
+{
+	return(stream << rational.GetNumerator() << '/'
+		  << rational.GetDenominator()
+		);
+	
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 14. Реализовать оператор ввода рационального числа из входного потока 
+//////////////////////////////////////////////////////////////////////////
+
+std::istream & operator >> (std::istream &stream, CRational & rational)
+{
+	unsigned numerator = 0;
+	unsigned denominator = 1;
+	if (
+		(stream >> numerator) && (stream.get() == '/') &&
+		(stream >> denominator)
+		)
+	{
+		rational = CRational(numerator, denominator);
+	}
+	else
+	{
+		stream.setstate(std::ios_base::failbit | stream.rdstate());
+	}
+	return stream;
+}
---
+++ lab5/task1tests/RationalTests.cpp
@@ -0,0 +1,253 @@
+﻿// task1tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../task1/Rational.h"
+
+BOOST_AUTO_TEST_CASE(Test_Greatest_Common_Denominator)
+{
+	BOOST_CHECK_EQUAL(GCD(2, 3), 1u);
+	BOOST_CHECK_EQUAL(GCD(3, 2), 1u);
+	BOOST_CHECK_EQUAL(GCD(12, 8), 4u);
+	BOOST_CHECK_EQUAL(GCD(8, 12), 4u);
+	BOOST_CHECK_EQUAL(GCD(0, 2), 2u);
+	BOOST_CHECK_EQUAL(GCD(2, 0), 2u);
+	BOOST_CHECK_EQUAL(GCD(0, 0), 1u);
+}
+
+void VerifyRational(const CRational & r, int expectedNumerator, int expectedDenominator)
+{
+	BOOST_CHECK_EQUAL(r.GetNumerator(), expectedNumerator);
+	BOOST_CHECK_EQUAL(r.GetDenominator(), expectedDenominator);
+}
+
+BOOST_AUTO_TEST_SUITE(Rational_number)
+	BOOST_AUTO_TEST_CASE(is_0_by_default)
+	{
+		VerifyRational(CRational(), 0, 1);
+	}
+	BOOST_AUTO_TEST_CASE(can_be_constructed_from_integer)
+	{
+		VerifyRational(CRational(10), 10, 1);
+		VerifyRational(CRational(-10), -10, 1);
+		VerifyRational(CRational(0), 0, 1);
+	}
+	BOOST_AUTO_TEST_CASE(can_be_constructed_with_numerator_and_denominator)
+	{
+		VerifyRational(CRational(5, 2), 5, 2);
+		VerifyRational(CRational(-5, 2), -5, 2);
+		VerifyRational(CRational(5, -2), -5, 2);
+		VerifyRational(CRational(-5, -2), 5, 2);
+	}
+	BOOST_AUTO_TEST_CASE(is_normalized_after_construction)
+	{
+		VerifyRational(CRational(6, 8), 3, 4);
+		VerifyRational(CRational(6, -8), -3, 4);
+		VerifyRational(CRational(-6, 8), -3, 4);
+		VerifyRational(CRational(-6, -8), 3, 4);
+		VerifyRational(CRational(-10, 20), -1, 2);
+	}
+	BOOST_AUTO_TEST_CASE(cant_have_zero_denominator)
+	{
+		BOOST_REQUIRE_THROW(CRational(1, 0), std::invalid_argument);
+	}
+
+	BOOST_AUTO_TEST_CASE(can_be_converted_to_double)
+	{
+	    CRational rational(2, 4);
+		BOOST_CHECK_CLOSE_FRACTION(rational.ToDouble(), 0.5, DBL_EPSILON);
+	}
+
+	BOOST_AUTO_TEST_SUITE(unar_operators)
+		BOOST_AUTO_TEST_CASE(unar_plus)
+		{
+			VerifyRational(+CRational(3, 5), 3, 5);
+		}
+		BOOST_AUTO_TEST_CASE(unar_minus)
+		{
+			VerifyRational(-CRational(6, 11), -6, 11);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(binary_operator_plus)
+		BOOST_AUTO_TEST_CASE(can_sum_two_rational_numbers)
+		{
+			VerifyRational(CRational(1, 2) + CRational(1, 6), 2, 3);
+		}
+
+		BOOST_AUTO_TEST_CASE(can_sum_rational_number_and_integer)
+		{
+			VerifyRational(CRational(1, 2) + 9, 19, 2);
+		}
+
+		BOOST_AUTO_TEST_CASE(can_sum_integer_and_rational_number)
+		{
+			VerifyRational(10 + CRational(1, 2), 21, 2);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+	
+	BOOST_AUTO_TEST_SUITE(binary_minus)
+		BOOST_AUTO_TEST_CASE(can_substract_two_rational_numbers)
+		{
+			VerifyRational(CRational(3, 5) - CRational(2, 5), 1, 5);
+		}
+		BOOST_AUTO_TEST_CASE(can_substract_rational_number_and_integer)
+		{
+			VerifyRational(CRational(1, 2) - CRational(2), -3, 2);
+		}
+		BOOST_AUTO_TEST_CASE(can_substract_integer_and_rational_number)
+		{
+			VerifyRational(CRational(2) - CRational(7, 5), 3, 5);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(operator_plus_equal)
+		BOOST_AUTO_TEST_CASE(can_sum_two_rational_numbers)
+		{
+			VerifyRational(CRational(1, 2) += CRational(1, 6), 2, 3);
+		}
+
+		BOOST_AUTO_TEST_CASE(can_sum_rational_number_and_integer)
+		{
+			VerifyRational(CRational(1, 2) += 9, 19, 2);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+	
+	BOOST_AUTO_TEST_SUITE(operator_minus_equal)
+		BOOST_AUTO_TEST_CASE(can_substract_two_rational_numbers)
+		{
+			VerifyRational(CRational(3, 5) -= CRational(2, 5), 1, 5);
+		}
+		BOOST_AUTO_TEST_CASE(can_substract_rational_number_and_integer)
+		{
+			VerifyRational(CRational(1, 2) -= CRational(2), -3, 2);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+	
+	BOOST_AUTO_TEST_SUITE(operator_multiply)
+		BOOST_AUTO_TEST_CASE(can_multiply_rational_number_and_integer)
+		{
+			VerifyRational(CRational(1, 6) * CRational(2), 1, 3);
+		}
+		BOOST_AUTO_TEST_CASE(can_multiply_two_rational_numbers)
+		{
+			VerifyRational(CRational(1, 6) * CRational(2, 7), 1, 21);
+		}
+		BOOST_AUTO_TEST_CASE(can_multiply_integer_and_rational_number)
+		{
+			VerifyRational(CRational(2) * CRational(1, 4), 1, 2);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+	BOOST_AUTO_TEST_SUITE(operator_multiply_equal)
+		BOOST_AUTO_TEST_CASE(can_multiply_rational_number_and_integer)
+		{
+			VerifyRational(CRational(1, 6) *= CRational(2), 1, 3);
+		}
+		BOOST_AUTO_TEST_CASE(can_multiply_two_rational_numbers)
+		{
+			VerifyRational(CRational(1, 6) *= CRational(2, 7), 1, 21);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(operator_division)
+		BOOST_AUTO_TEST_CASE(can_divide_rational_number_by_integer)
+		{
+			VerifyRational(CRational(1, 6) / CRational(2), 1, 12);
+		}
+		BOOST_AUTO_TEST_CASE(can_divide_rational_number_by_rational_number)
+		{
+			VerifyRational(CRational(1, 6) / CRational(2, 7), 7, 12);
+		}
+		BOOST_AUTO_TEST_CASE(can_divide_integer_by_rational_number)
+		{
+			VerifyRational(CRational(2) / CRational(1, 4), 8, 1);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(operator_division_equal)
+		BOOST_AUTO_TEST_CASE(can_divide_rational_number_by_integer)
+		{
+			VerifyRational(CRational(1, 6) /= CRational(2), 1, 12);
+		}
+		BOOST_AUTO_TEST_CASE(can_divide_rational_number_by_rational_number)
+		{
+			VerifyRational(CRational(1, 6) /= CRational(2, 7), 7, 12);
+		}
+		BOOST_AUTO_TEST_CASE(can_divide_integer_by_rational_number)
+		{
+			VerifyRational(CRational(2) /= CRational(1, 4), 8, 1);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+
+	BOOST_AUTO_TEST_SUITE(operator_equal)
+		BOOST_AUTO_TEST_CASE(compares_equal)
+		{
+			BOOST_CHECK(CRational(1, 6) == CRational(1, 6));
+			BOOST_CHECK(4 == CRational(4, 1));
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(operator_unequal)
+		BOOST_AUTO_TEST_CASE(compares_unequal)
+		{
+			BOOST_CHECK(CRational(1, 3) != CRational(4, 2));
+			BOOST_CHECK(CRational(3, 4) != CRational(3, 8));
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(compare_operators)
+		BOOST_AUTO_TEST_CASE(less)
+		{
+			BOOST_CHECK(CRational(1, 4) < CRational(1, 2));
+		}
+		BOOST_AUTO_TEST_CASE(less_equal)
+		{
+			BOOST_CHECK(CRational(1, 4) <= CRational(1, 2));
+			BOOST_CHECK(CRational(1, 4) <= CRational(1, 4));
+		}
+		BOOST_AUTO_TEST_CASE(more)
+		{
+			BOOST_CHECK(CRational(1, 2) > CRational(1, 3));
+		}
+		BOOST_AUTO_TEST_CASE(more_equal)
+		{
+			BOOST_CHECK(CRational(6, 3) >= 2);
+			BOOST_CHECK(CRational(2, 3) >= CRational(1, 6));
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 13. Реализовать оператор вывода рационального числа в выходной поток 
+//	std::ostream в формате <числитель>/<знаменатель>, 
+//	например: 7/15
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_SUITE(output_operator)
+		BOOST_AUTO_TEST_CASE(can_write_rational_number_to_ostream)
+		{
+			boost::test_tools::output_test_stream output;
+			output << CRational(4, 7);
+			BOOST_CHECK(output.is_equal("4/7"));
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+
+//////////////////////////////////////////////////////////////////////////
+// TODO: 14. Реализовать оператор ввода рационального числа из входного потока 
+//	std::istream в формате <числитель>/<знаменатель>, 
+//	например: 7/15
+//////////////////////////////////////////////////////////////////////////
+
+	BOOST_AUTO_TEST_SUITE(input_operator)
+		BOOST_AUTO_TEST_CASE(can_read_rational_number_from_istream)
+		{
+			std::istringstream stream("1/2");
+			CRational rational;
+			stream >> rational;
+			VerifyRational(rational, 1, 2);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
